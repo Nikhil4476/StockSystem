c8883596b4030f3ceed916448f299c889135dff3
Nikhil Kamleshkumar <P2424383@LEC-ADMIN.dmu.ac.uk>
2020-02-04 14:02:54 +0000

    Add project files.

---
+++ StockClasses/clsDataConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockClasses
+{
+    public class clsDataConnection
+    {
+        //connection object used to connect to the database
+        SqlConnection connectionToDB = new SqlConnection();
+        //data adpater used to transfer data to and from the database 
+        SqlDataAdapter dataChannel = new SqlDataAdapter();
+    }
+    set
+    {
+        //set the query results 
+        dataTable = value;
+    }
+}
+
+
---
+++ StockTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StockTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StockTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bc7994cd-360c-4573-b7c9-fcde5f5ca16c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
